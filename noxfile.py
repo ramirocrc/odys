99359f87bc7c2621b6fcd2121e5502e011e81865
Ramiro Criach <ramirocriach@mac.home>
2025-11-29 17:54:42 +0100

    test lowesta and highest dependencies

--- noxfile.py
+++ noxfile.py
@@ -9,15 +9,45 @@ PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13", "3.14"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
-def tests(session: nox.Session) -> None:
+def test_integration_highest(session: nox.Session) -> None:
     """Run the test suite with coverage reporting.
 
     Args:
         session: The nox session object.
     """
     session.install("uv")
-    session.run("uv", "sync", "--active")
-    session.run("python", "-c", "import sys; print(f'Running tests on Python {sys.version}')")
+    session.run("uv", "pip", "install", "-e", ".", "--resolution", "highest")
+    session.run(
+        "python",
+        "-c",
+        "import sys; print(f'Running tests on Python {sys.version} (highest resolution)')",
+    )
+    session.run(
+        "uv",
+        "run",
+        "python",
+        "-m",
+        "pytest",
+        "-n",
+        "auto",
+        "tests/integration",
+    )
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def test_integration_lowest(session: nox.Session) -> None:
+    """Run the test suite with coverage reporting.
+
+    Args:
+        session: The nox session object.
+    """
+    session.install("uv")
+    session.run("uv", "pip", "install", "-e", ".", "--resolution", "lowest-direct")
+    session.run(
+        "python",
+        "-c",
+        "import sys; print(f'Running tests on Python {sys.version} (highest resolution)')",
+    )
     session.run(
         "uv",
         "run",
